65465576e32116a7cc13e19ff00d3bb6bce3e317
Mike Casey [DartCS] <mcasey@dartmouth.edu>
2015-01-11 10:16:58 -0500

    update to suite to include module references

--- bregman/suite.py
+++ bregman/suite.py
@@ -1,6 +1,8 @@
 import os,sys,glob
 import pylab
 import bregman
+import os.path
+
 from features import *
 from segment import *
 from audiodb import *
@@ -14,9 +16,22 @@ from classifier import *
 from error import *
 from beat import *
 
+import features
+import segment
+import testsignal
+import psychoacoustics
+import tuning
+import sound
+import plca
+import distance
+import classifier
+import error
+import beat
+
+sep = os.path.sep
 bregman_data_root = os.path.split(bregman.__file__)[0]
-examples_dir = os.path.join(bregman_data_root,"examples/")
-audio_dir = os.path.join(bregman_data_root,"audio/")
+examples_dir = os.path.join(bregman_data_root,"examples"+sep)
+audio_dir = os.path.join(bregman_data_root,"audio"+sep)
 sys.path.append(examples_dir)
 
 def get_tutorials():
